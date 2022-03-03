7d449595d6c9ae8bf5cc5740cc584540dc7aed84
Rafał Korman <rafalkor5@gmail.com>
2022-03-03 15:26:15 +0100

    Add Singleton Pattern

---
+++ src/main/java/Singleton/GameEngine.java
@@ -0,0 +1,31 @@
+package Singleton;
+
+public class GameEngine {
+    private int hp = 100;
+    private String characterName = "";
+    //pole statyczne z dostępem bez konieczności tworzenia obiektu
+    private static GameEngine instance;
+    //prywatny konstruktor
+    private GameEngine() {
+
+    }
+
+    public void run(){
+        while(true){
+            //czekamy na input gracza
+            //zmieniamy stan gry
+            //renderujemy grafike
+        }
+    }
+    //metoda tworząca silnik oraz zabezpieczająca przed jego wielokrotnym utworzeniem
+    public static GameEngine getInstance(){
+        if(instance == null){
+            //chronienie wielowątkowości
+            synchronized (GameEngine.class){
+                if(instance == null) instance = new GameEngine();
+            }
+
+        }
+        return instance;
+    }
+}
---
+++ src/main/java/Singleton/Main.java
@@ -0,0 +1,10 @@
+package Singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        //utworzenie silnika
+        GameEngine engine = GameEngine.getInstance();
+        //uruchomienie silnika
+        engine.run();
+    }
+}
